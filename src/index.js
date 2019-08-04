4f6431cafe32a1770a9390be39db25d959df3426
Cordell <cordell-smith@outlook.com>
2019-08-04 10:55:47 +0800

    Removed server file system. Was not needed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => 
+  res.redirect('/home')
+);
+
+app.get('/home', (req, res) => 
+  res.send('<h1>Welcome!!</h1>')
+);
+
+app.get('/about', (req, res) => 
+  res.send('Admin page')
+);
+
+// Prints a log once the server starts listening
+app.listen(port, () => 
+  console.log(`Server running on http://localhost:${port}/`))
+;
