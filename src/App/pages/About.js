b3d660dbf2c3022499b070f094a532c9aebe0fef
Cordell Smith <cordell-smith@outlook.com>
2019-08-08 13:55:39 +0800

    Filtering out stock css and making my own.

--- src/App/pages/About.js
+++ src/App/pages/About.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 function About() {
   return(
-    <div>
-      <div className="w3-black" id="about">
-        <div className="w3-container w3-content w3-padding-64" style={{maxWidth:'800px'}}>
-          <h2 className="w3-wide w3-center">ABOUT ME</h2>
-          <p className="w3-opacity w3-center"><i>Everything you need to know</i></p><br/>
-        </div>
+    <div id="coco-container">
+      <div id="coco-content">
+        <h2>ABOUT ME</h2>
+        <i>Everything you need to know</i>
       </div>
     </div>
   );
--- src/App/pages/Contact.js
+++ src/App/pages/Contact.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 function Contact() {
   return(
-    <div>
-      <div className="w3-black" id="about">
-        <div className="w3-container w3-content w3-padding-64" style={{maxWidth:'800px'}}>
-          <h2 className="w3-wide w3-center">CONTACT</h2>
-          <p className="w3-opacity w3-center"><i>cordell-smith@outlook.com</i></p><br/>
-        </div>
+    <div id="coco-container">
+      <div id="coco-content">
+        <h2>CONTACT</h2>
+        <i>cordell-smith@outlook.com</i>
       </div>
     </div>
   );
