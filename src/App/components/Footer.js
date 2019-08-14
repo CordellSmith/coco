d62a028c740e06fe48eca263471e1f15c9f771f3
Cordell <cordell-smith@outlook.com>
2019-08-14 21:26:29 +0800

    Added a google font and more messing around with styling.

--- src/App/components/Footer.js
+++ src/App/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
       <div>
         <div className="coco-footer">
           <div className="media-icons">
-            <div className="media-icon facebook-mi"></div>
+            <div className="media-icon facebook-mi opacity70"></div>
             <div className="media-icon linkedin-mi"></div>
             <div className="media-icon github-mi"></div>
             <div className="media-icon instagram-mi"></div>
