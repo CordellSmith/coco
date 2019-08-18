2b19ae5b708f19ff2325b92c990eadd629c2e5f0
Cordell <cordell-smith@outlook.com>
2019-08-18 21:31:02 +0800

    Cleaned up some css.

--- src/App/components/Navbar.js
+++ src/App/components/Navbar.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <ul>
-      <li><Link to="/"><span id="navbar-text">Home</span></Link></li>
-      <li><Link to="/portfolio"><span id="navbar-text">Portfolio</span></Link></li>
-      <li><Link to="/about"><span id="navbar-text">About</span></Link></li>
-      <li><Link to="/contact"><span id="navbar-text">Contact</span></Link></li>
+      <li className="nav-home"><Link to="/"><span id="navbar-text">Home</span></Link></li>
+      <li className="nav-portfolio"><Link to="/portfolio"><span id="navbar-text">Portfolio</span></Link></li>
+      <li className="nav-about"><Link to="/about"><span id="navbar-text">About</span></Link></li>
+      <li className="nav-contact"><Link to="/contact"><span id="navbar-text">Contact</span></Link></li>
     </ul>
   );
 }
